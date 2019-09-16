e8532e7584e8b1c5c556e7f46e1d0513f71dc19d
Nien <wolfzxcv@gmail.com>
2019-09-16 20:14:38 +0100

    Updates

---
+++ precache-manifest.c746a6b598b69467a7ebd32f23e1bbd6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfd9935d8b80ce7c616780af79addb3b",
+    "url": "/Sports-Poll/index.html"
+  },
+  {
+    "revision": "8a120f4f1b49b8c0b370",
+    "url": "/Sports-Poll/static/css/main.4bbcf444.chunk.css"
+  },
+  {
+    "revision": "7ce7f756cebb8b64a7a2",
+    "url": "/Sports-Poll/static/js/2.0fcde91c.chunk.js"
+  },
+  {
+    "revision": "8a120f4f1b49b8c0b370",
+    "url": "/Sports-Poll/static/js/main.739ed875.chunk.js"
+  },
+  {
+    "revision": "8b50a6443689be7cfe4b",
+    "url": "/Sports-Poll/static/js/runtime~main.83636eba.js"
+  },
+  {
+    "revision": "d4317ec0980867f64c556d44bc5efb05",
+    "url": "/Sports-Poll/static/media/bg.d4317ec0.jpg"
+  }
+]);
